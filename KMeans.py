d1c822c83bbb1a3cc9e9bea82e57cb3ff6e0b167
rapcode005 <32093759+rapcode005@users.noreply.github.com>
2019-07-31 01:00:10 +0800

    Update KMeans.py

--- KMeans.py
+++ KMeans.py
@@ -29,5 +29,3 @@ def solution(arr):
             c2 = [sum([z[0] for z in cluster_c2]) / len(cluster_c2), sum([z[1] for z in cluster_c2]) / len(cluster_c2)]
 
     return "Cluster coordinates of centroids " + str(c1) + ": " + str(cluster_c1) + "\nCluster coordinates of centroids " + str(c2) + ": " + str(cluster_c2)
-
-print(solution([[5,3], [10,15], [15,12], [24,10], [30,45], [85,70], [71,80], [60,78], [55,52], [80,91]]))
